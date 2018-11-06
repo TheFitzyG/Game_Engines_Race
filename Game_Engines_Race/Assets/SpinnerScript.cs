d80c9e795fbd476611e37c4bd2c641337f4f98b4
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2018-11-06 20:41:24 +0000

    co

---
+++ Game_Engines_Race/Assets/SpinnerScript.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using UnityEngine;
+using UnityEngine.Assertions.Comparers;
+
+
+public class SpinnerScript : MonoBehaviour {
+
+	public GameObject EndPiece;
+	
+	[Header("Position")]
+	public float Elevation;
+	public float Radius;
+	
+	[Header("Movement")]
+	public float RotationSpeed;
+	public float Offset;
+	private float theta;
+
+	[Space] public bool SinElevation;
+	private float elevationTheta;
+	public float ElevationRange;
+	public float ElevationChangeSpeed;
+
+	[Header("Visual")] public bool ShowLine;
+	private LineRenderer LR;
+
+	[Header("Randomisation")]
+	public bool RandomiseSpeed;
+	public float MinSpeed;
+	public float MaxSpeed;
+
+	
+
+	// Use this for initialization
+	void Start () {
+		
+		
+		
+		
+		EndPiece.transform.localPosition = new Vector3(Radius, Elevation, 0f).normalized * Radius;
+		theta = Offset;
+
+		if (ShowLine) {
+
+			LR = GetComponent<LineRenderer>();
+UpdateLine();
+
+
+		}
+		
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		
+		transform.rotation = Quaternion.AngleAxis(theta, transform.up);
+
+		theta += (RotationSpeed * Time.deltaTime);
+
+
+		if (SinElevation) {
+
+			float newElevation =  Elevation + (ElevationRange * Mathf.Sin(elevationTheta)); 
+			
+			
+			
+
+			EndPiece.transform.localPosition = new Vector3(Radius, newElevation, 0f).normalized * Radius;
+
+
+			elevationTheta += (ElevationChangeSpeed * Time.deltaTime);
+
+		}
+
+		if (ShowLine) {
+
+			UpdateLine();
+			
+
+		}
+
+
+
+	}
+
+
+	void UpdateLine() {
+
+
+		LR.numPositions = 2;
+		LR.SetPosition(0, transform.position); 
+		LR.SetPosition(1, EndPiece.transform.position);
+
+
+
+
+	}
+
+}
